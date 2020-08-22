91d2f10830c96348d0e79ef66eb1192d7fd6296b
roby <roby@ipac.caltech.edu>
2020-08-21 19:10:18 -0600

    BUG FIX: fixed a refactor issue

--- src/suit/js/SUIT.js
+++ src/suit/js/SUIT.js
@@ -7,8 +7,7 @@ import {get} from 'lodash';
 
 import {firefly} from 'firefly/Firefly.js';
 import {timeSeriesButton} from './actions.jsx';
-import {mergeObjectOnly} from 'firefly/util/WebUtil.js';
-import {getRootURL} from 'firefly/util/BrowserUtil.js';
+import {mergeObjectOnly, getRootURL} from 'firefly/util/WebUtil.js';
 import {showInfoPopup} from 'firefly/ui/PopupUtil.jsx';
 import './suit.css';
 
