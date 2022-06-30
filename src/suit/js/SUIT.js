79b6fbc42f4cb8435f6d894c51eefce883381fa9
Gregory Dubois-Felsmann <gpdf@ipac.caltech.edu>
2022-06-30 10:54:48 -0700

    Update examples for DC2

--- src/suit/js/SUIT.js
+++ src/suit/js/SUIT.js
@@ -56,28 +56,41 @@ const lsstEntry= (label, url) => (
         value: url,
         examples: [
             {
-                description: 'Query the object table to get positions and composite model magnitudes and their errors in three filters using a CONE search to define a region on the sky.  Filter on good sources  only',
+                description: 'Query the object table to get positions and composite model magnitudes and their errors in three filters using a CONE search to define a region on the sky. Filter on deblended sources with i-band magnitudes brighter than 25 mag.',
                 statement:
-`SELECT objectId, ra, dec, good, mag_g_cModel as gmag, mag_i_cModel as imag, mag_r_cModel as rmag,
-magerr_g_cModel as gmag_err, magerr_i_cModel as imag_err, magerr_r_cModel as rmag_err
-FROM dp01_dc2_catalogs.object
-WHERE CONTAINS (POINT('ICRS', ra, dec), CIRCLE('ICRS', 62.0, -37.0, 0.1 )) = 1 AND good = 1`
+`SELECT objectId, coord_ra, coord_dec, detect_isPrimary, 
+scisql_nanojanskyToAbMag(g_cModelFlux) as gmag, scisql_nanojanskyToAbMag(i_cModelFlux) as imag,
+scisql_nanojanskyToAbMag(r_cModelFlux) as rmag,
+scisql_nanojanskyToAbMagSigma(g_cModelFlux, g_cModelFluxErr) as gmag_err,
+scisql_nanojanskyToAbMagSigma(i_cModelFlux, i_cModelFluxErr) as imag_err,
+scisql_nanojanskyToAbMagSigma(r_cModelFlux, r_cModelFluxErr) as rmag_err
+FROM dp02_dc2_catalogs.Object
+WHERE CONTAINS (POINT('ICRS', coord_ra, coord_dec), CIRCLE('ICRS', 62.0, -37.0, 0.05)) = 1
+AND detect_isPrimary = 1`
             },
             {
-                description: 'Select all SNe from the truth match table at redshift < 1',
+                description: 'Jointly query the DiaSource and CcdVisit tables to obtain the magnitudes of all of the i-band detections in difference images (DiaSources) for a time-domain event within 2 arcsec of (67.4579, -44.0802), and the associated seeing and visitId.',
                 statement:
-`SELECT * FROM dp01_dc2_catalogs.truth_match
-WHERE dp01_dc2_catalogs.truth_match.truth_type = 3
-AND dp01_dc2_catalogs.truth_match.redshift < 1`
+`SELECT diasrc.ra, diasrc.decl, diasrc.diaObjectId, diasrc.diaSourceId, diasrc.filterName, diasrc.midPointTai,
+scisql_nanojanskyToAbMag(diasrc.psFlux) AS psAbMag,
+ccdvis.seeing, ccdvis.visitId
+FROM dp02_dc2_catalogs.DiaSource AS diasrc JOIN dp02_dc2_catalogs.CcdVisit AS ccdvis
+ON diasrc.ccdVisitId = ccdvis.ccdVisitId
+WHERE CONTAINS(POINT('ICRS', diasrc.ra, diasrc.decl), CIRCLE('ICRS', 67.4579, -44.0802, 0.0006))=1
+AND diasrc.filterName = 'i'`
             },
             {
-                description: 'Execute a cone search centered on (RA, Dec) = (61.863, -35.79) with a radius of 20 arcseconds and applying a cut on magnitude. Join with the truth catalog  and add in some quality filters on the match. Apply quality cuts on the match to return only those objects with a match in the truth catalog. Filter also on  sources satisfying the “is_good_match” flag',
+                description: 'Query for processed visit images (calexps) that overlap the coordinate (62, -37) and were obtained in the r-band filter between modified Julian dates 60900 and 61000.',
                 statement:
-`SELECT obj.objectId, obj.ra, obj.dec, obj.mag_r_cModel, obj.cModelFlux_r, truth.mag_r, truth.flux_r, truth.truth_type
-FROM dp01_dc2_catalogs.object as obj JOIN dp01_dc2_catalogs.truth_match as truth ON truth.match_objectId = obj.objectId
-WHERE CONTAINS(POINT('ICRS', obj.ra, obj.dec),CIRCLE('ICRS', 62.0, -37.0, 0.1 )) = 1
-AND truth.match_objectid >= 0 
-AND truth.is_good_match = 1`,
+`SELECT dataproduct_type, dataproduct_subtype, calib_level, lsst_band, em_min, em_max, 
+lsst_tract, lsst_patch, lsst_visit, lsst_filter, lsst_detector, t_exptime, t_min, t_max, s_ra, s_dec, s_fov, 
+obs_id, obs_collection, o_ucd, facility_name, instrument_name, s_region, access_url, access_format 
+FROM ivoa.ObsCore 
+WHERE CONTAINS(POINT('ICRS', 62, -37), s_region)=1 
+AND calib_level = 2 
+AND dataproduct_type = 'image' 
+AND ( t_min <= 61000 AND 60900 <= t_max ) 
+AND ( 600e-9 BETWEEN em_min AND em_max )`
             },
         ]
     });
