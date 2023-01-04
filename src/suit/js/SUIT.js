6032060c4704bc3278c3c9e590dca9f7b57d6970
roby <roby@ipac.caltech.edu>
2023-01-04 13:17:18 -0700

    suit-release-2022.6
      - removed suit-docker.env options
      - suit-release: update some config settings
      - suit-release-2022-6 config: disabled workspace option
      - Added click to action searches
      - cleaned up text

--- src/suit/js/SUIT.js
+++ src/suit/js/SUIT.js
@@ -2,13 +2,15 @@
  * License information at https://github.com/Caltech-IPAC/firefly/blob/master/License.txt
  */
 import {firefly} from 'firefly/Firefly.js';
+import {
+    makeDefImageSearchActions,
+    makeDefTableSearchActions, makeDefTapSearchActions, makeExternalSearchActions
+} from 'firefly/ui/DefaultSearchActions.js';
 import {mergeObjectOnly, getRootURL} from 'firefly/util/WebUtil.js';
-import {showInfoPopup} from 'firefly/ui/PopupUtil.jsx';
-import {makeWorldPt} from 'firefly/visualize/Point.js';
 import {getTAPServices} from 'firefly/ui/tap/TapKnownServices.js';
 import {getFireflyViewerWebApiCommands} from 'firefly/api/webApiCommands/ViewerWebApiCommands.js';
-import {getDefaultMOCList} from 'firefly/visualize/HiPSMocUtil.js';
 import './suit.css';
+import {makeLsstClickToAction, makeLsstTapEntry} from './actions.jsx';
 
 // import SUIT_ICO from 'html/images/rubin_logo_transparent-70.png';
 
@@ -29,61 +31,17 @@ let props = {
         {label: 'External Catalogs', action: 'MultiTableSearchCmd'},
         {label: 'Add Chart', action: 'ChartSelectDropDownCmd'},
         {label: 'Upload', action: 'FileUploadDropDownCmd'}
-    ]
+    ],
 };
+
+const LSST_TAP_LABEL= 'LSST RSP';
 
 props = mergeObjectOnly(props, window.firefly?.app ?? {});
 
-const lsstEntry=
-    {
-        label:'LSST RSP',
-        value: 'https://data-int.lsst.cloud/api/tap',
-        fovDeg: 10,
-        centerWP: makeWorldPt(62,-37).toString(),
-        hipsUrl: 'https://irsa.ipac.caltech.edu/data/hips/list',
-        examples: [
-            {
-                description: 'Query the object table to get positions and composite model magnitudes and their errors in three filters using a CONE search to define a region on the sky. Filter on deblended sources with i-band magnitudes brighter than 25 mag.',
-                statement:
-`SELECT objectId, coord_ra, coord_dec, detect_isPrimary, 
-scisql_nanojanskyToAbMag(g_cModelFlux) as gmag, scisql_nanojanskyToAbMag(i_cModelFlux) as imag,
-scisql_nanojanskyToAbMag(r_cModelFlux) as rmag,
-scisql_nanojanskyToAbMagSigma(g_cModelFlux, g_cModelFluxErr) as gmag_err,
-scisql_nanojanskyToAbMagSigma(i_cModelFlux, i_cModelFluxErr) as imag_err,
-scisql_nanojanskyToAbMagSigma(r_cModelFlux, r_cModelFluxErr) as rmag_err
-FROM dp02_dc2_catalogs.Object
-WHERE CONTAINS (POINT('ICRS', coord_ra, coord_dec), CIRCLE('ICRS', 62.0, -37.0, 0.05)) = 1
-AND detect_isPrimary = 1`
-            },
-            {
-                description: 'Jointly query the DiaSource and CcdVisit tables to obtain the magnitudes of all of the i-band detections in difference images (DiaSources) for a time-domain event within 2 arcsec of (67.4579, -44.0802), and the associated seeing and visitId.',
-                statement:
-`SELECT diasrc.ra, diasrc.decl, diasrc.diaObjectId, diasrc.diaSourceId, diasrc.filterName, diasrc.midPointTai,
-scisql_nanojanskyToAbMag(diasrc.psFlux) AS psAbMag,
-ccdvis.seeing, ccdvis.visitId
-FROM dp02_dc2_catalogs.DiaSource AS diasrc JOIN dp02_dc2_catalogs.CcdVisit AS ccdvis
-ON diasrc.ccdVisitId = ccdvis.ccdVisitId
-WHERE CONTAINS(POINT('ICRS', diasrc.ra, diasrc.decl), CIRCLE('ICRS', 67.4579, -44.0802, 0.0006))=1
-AND diasrc.filterName = 'i'`
-            },
-            {
-                description: 'Query for processed visit images (calexps) that overlap the coordinate (62, -37) and were obtained in the r-band filter between modified Julian dates 60900 and 61000.',
-                statement:
-`SELECT dataproduct_type, dataproduct_subtype, calib_level, lsst_band, em_min, em_max, 
-lsst_tract, lsst_patch, lsst_visit, lsst_filter, lsst_detector, t_exptime, t_min, t_max, s_ra, s_dec, s_fov, 
-obs_id, obs_collection, o_ucd, facility_name, instrument_name, s_region, access_url, access_format 
-FROM ivoa.ObsCore 
-WHERE CONTAINS(POINT('ICRS', 62, -37), s_region)=1 
-AND calib_level = 2 
-AND dataproduct_type = 'image' 
-AND ( t_min <= 61000 AND 60900 <= t_max ) 
-AND ( 600e-9 BETWEEN em_min AND em_max )`
-            },
-        ]
-    };
+
 
 const tapServices=  [
-    lsstEntry,
+    makeLsstTapEntry(),
     ...getTAPServices( ['IRSA', 'Gaia', 'CADC', 'MAST Images', 'GAVO', 'HSA', 'NED', 'NASA Exoplanet Archive'])
 ];
 
@@ -121,6 +79,41 @@ let options = {
         services: tapServices,
         defaultMaxrec: 50000
     },
+    tapObsCore: {
+        [LSST_TAP_LABEL]  : {
+            filterDefinitions: [
+                {
+                    name: 'LSSTCam',
+                    options: [
+                        {label: 'u', value : '367', title: '367nm central value'},
+                        {label: 'g', value : '483', title: '483nm central value'},
+                        {label: 'r', value : '622', title: '622nm central value'},
+                        {label: 'i', value : '755', title: '755nm central value'},
+                        {label: 'z', value : '869', title: '869nm central value'},
+                        {label: 'y', value : '971', title: '971nm central value'},
+                    ],
+                },
+            ],
+            obsCoreCalibrationLevel: {
+                tooltip: 'Calibration Level',
+                helptext: '1 is raw data; 2 is PVIs; 3 includes coadds and difference images',
+                level: {
+                    1: {title: 'For Rubin: Raw Data'},
+                    2: {title: 'For Rubin: PVIs'},
+                    3: {title: 'For Rubin: Coadds and Difference Images'},
+                },
+            },
+            obsCoreInstrumentName: {
+                tooltip: 'LSSTCam, LSSTCam-imSim',
+                placeholder: 'e.g. LSSTCam-imSim',
+            },
+            obsCoreSubType: {
+                tooltip: 'Specific type of image or other dataset',
+                placeholder: 'e.g. lsst.deepCoadd_calexp',
+                helptext: '"lsst." + Butler Repo Dataset type',
+            },
+        }
+    },
     hips: {
         readoutShowsPixel : true,
         hipsSources: 'lsst,cds',
@@ -135,11 +128,25 @@ let options = {
             label: 'Rubin Featured MOC '
         },
     },
-    workspace: {showOptions: true},
+    // workspace: {showOptions: true},
     /* eslint-disable quotes */
     targetPanelExampleRow1: [`'62, -37'`, `'60.4 -35.1'`, `'4h11m59s -32d51m59s equ j2000'`, `'239.2 -47.6 gal'`],
     targetPanelExampleRow2: [`'NGC 1532' (NB: DC2 is a simulated sky, so names are not useful)`],
     /* eslint-enable quotes */
+    searchActions : [
+        ...makeExternalSearchActions(),
+        ...makeDefTableSearchActions(),
+        ...makeDefTapSearchActions(),
+        ...makeDefImageSearchActions(),
+        ...makeLsstClickToAction(),
+    ],
+    searchActionsCmdMask: [
+        'nedRadius', 'simbadRadius', 'gotoSimbadRadius',
+        'tableNed', 'tableSimbad', 'tableSimbadGoto', 'imageFits', 'tableHiPS',
+        'tapRadius', 'tapArea', 'tableTapRadius',
+        'imageFits', 'HiPS', 'lsstObsCoreTap', 'lsstTruthSummaryRadius', 'lsstTruthSummaryArea',
+        'lsstObsCoreTapTable', 'lsstTruthSummaryRadiusTable'
+    ],
 };
 
 options = mergeObjectOnly(options, window.firefly?.options ?? {});
--- src/suit/js/actions.jsx
+++ src/suit/js/actions.jsx
@@ -1,26 +1,28 @@
+import {makeSearchAction, SearchTypes} from 'firefly/core/ClickToAction.js';
+import {showTapSearchPanel} from 'firefly/ui/DefaultSearchActions.js';
+import {getTapServices} from 'firefly/ui/tap/TapUtil.js';
+import {makeWorldPt} from 'firefly/visualize/Point.js';
 import React from 'react';
-import {get, set, has, cloneDeep, omitBy, isNil} from 'lodash';
+import {get, omitBy, isNil} from 'lodash';
 import {ActiveTableButton} from 'firefly/tables/ui/ActiveTableButton.jsx';
 import {getTblById, getTblRowAsObj} from 'firefly/tables/TableUtil.js';
-import {makeTblRequest} from 'firefly/tables/TableRequestUtil.js';
-import {getViewer} from 'firefly/api/ApiViewer.js';
 import {LC} from 'firefly/templates/lightcurve/LcManager.js';
 
 
-export function timeSeriesButton() {
-    return (
-        <div key='launchTimeSeries'>
-            <ActiveTableButton
-                title = 'Launch Time Series Viewer'
-                type = 'highlight'
-                onChange = {checkForTimeSeriesData}
-                onClick = {() => console.log('add a new launchTimeSeries function')}
-                style= {{maxWidth: 220}}
-            />
-        </div>
-    );
-}
-
+// export function timeSeriesButton() {
+//     return (
+//         <div key='launchTimeSeries'>
+//             <ActiveTableButton
+//                 title = 'Launch Time Series Viewer'
+//                 type = 'highlight'
+//                 onChange = {checkForTimeSeriesData}
+//                 onClick = {() => console.log('add a new launchTimeSeries function')}
+//                 style= {{maxWidth: 220}}
+//             />
+//         </div>
+//     );
+// }
+//
 // function launchTimeSeries({tbl_id, highlightedRow}) {
 //     const tableModel = getTblById(tbl_id) || {};
 //     let req;
@@ -100,3 +102,94 @@ function isTimeSeriesTable(tableModel) {
                  .findIndex( (k) => [LC.META_TIME_CNAME, LC.META_FLUX_CNAME, LC.META_MISSION].includes(k)) >=0;
 
 }
+
+const LSST_TAP_LABEL= 'LSST RSP';
+
+function getLsstTapServiceUrl() {
+    const url= getTapServices().filter( ({label}) => label===LSST_TAP_LABEL)?.[0]?.value;
+    return url;
+}
+
+export function makeLsstClickToAction() {
+
+    return [
+        makeSearchAction('lsstObsCoreTap', 'lsst-tap', 'ObsTAP for Images', 'Search ObsTAP', SearchTypes.point, .001, 2.25,
+            (sa, cenWpt, radius) => {
+                showTapSearchPanel( {wp: cenWpt, serviceUrl: getLsstTapServiceUrl(),
+                    schema:'ivoa', table:'ivoa.ObsCore'});
+            }),
+        makeSearchAction('lsstTruthSummaryRadius', 'lsst-tap', 'Truth Summary TAP ', 'Search truth summary', SearchTypes.pointRadius, .001, 2.25,
+            (sa, cenWpt, radius) => {
+                showTapSearchPanel( {wp: cenWpt, radiusInArcSec: radius, serviceUrl: getLsstTapServiceUrl(),
+                    schema:'dp02_dc2_catalogs', table:'dp02_dc2_catalogs.TruthSummary'});
+            }),
+        makeSearchAction('lsstTruthSummaryArea', 'lsst-tap', 'Truth Summary TAP ', 'Search truth summary', SearchTypes.area, undefined, undefined,
+            (sa, cenWpt, radius, corners) => {
+                showTapSearchPanel( {corners, serviceUrl: getLsstTapServiceUrl(),
+                    schema:'dp02_dc2_catalogs', table:'dp02_dc2_catalogs.TruthSummary'});
+            }, 'Search TruthSummary table within the specified polygon'),
+        makeSearchAction('lsstObsCoreTapTable', 'lsst_tap', 'Obscore TAP', 'TAP Search',
+            SearchTypes.point_table_only, undefined, undefined,
+            (sa, wp) => showTapSearchPanel( {wp, serviceUrl: getLsstTapServiceUrl(), schema:'ivoa', table:'ivoa.ObsCore'}),
+            'Search ObsTAP for images at row'),
+        makeSearchAction('lsstTruthSummaryRadiusTable', 'lsst_tap', 'TAP', 'Truth Summary TAP',
+            SearchTypes.point_table_only, undefined, undefined,
+            (sa, wp) => showTapSearchPanel( {wp, radiusInArcSec: 5/3600,
+                serviceUrl: getLsstTapServiceUrl(), schema:'dp02_dc2_catalogs', table:'dp02_dc2_catalogs.TruthSummary'}),
+            'Search Truth Summary table with 5" radius' ),
+    ];
+}
+
+
+
+
+export function makeLsstTapEntry() {
+
+    return (
+        {
+            label: LSST_TAP_LABEL,
+            value: 'https://data-int.lsst.cloud/api/tap',
+            fovDeg: 10,
+            centerWP: makeWorldPt(62,-37).toString(),
+            hipsUrl: 'https://irsa.ipac.caltech.edu/data/hips/list',
+            examples: [
+                {
+                    description: 'Query the object table to get positions and composite model magnitudes and their errors in three filters using a CONE search to define a region on the sky. Filter on deblended sources with i-band magnitudes brighter than 25 mag.',
+                    statement:
+                        `SELECT objectId, coord_ra, coord_dec, detect_isPrimary, 
+scisql_nanojanskyToAbMag(g_cModelFlux) as gmag, scisql_nanojanskyToAbMag(i_cModelFlux) as imag,
+scisql_nanojanskyToAbMag(r_cModelFlux) as rmag,
+scisql_nanojanskyToAbMagSigma(g_cModelFlux, g_cModelFluxErr) as gmag_err,
+scisql_nanojanskyToAbMagSigma(i_cModelFlux, i_cModelFluxErr) as imag_err,
+scisql_nanojanskyToAbMagSigma(r_cModelFlux, r_cModelFluxErr) as rmag_err
+FROM dp02_dc2_catalogs.Object
+WHERE CONTAINS (POINT('ICRS', coord_ra, coord_dec), CIRCLE('ICRS', 62.0, -37.0, 0.05)) = 1
+AND detect_isPrimary = 1`
+                },
+                {
+                    description: 'Jointly query the DiaSource and CcdVisit tables to obtain the magnitudes of all of the i-band detections in difference images (DiaSources) for a time-domain event within 2 arcsec of (67.4579, -44.0802), and the associated seeing and visitId.',
+                    statement:
+                        `SELECT diasrc.ra, diasrc.decl, diasrc.diaObjectId, diasrc.diaSourceId, diasrc.filterName, diasrc.midPointTai,
+scisql_nanojanskyToAbMag(diasrc.psFlux) AS psAbMag,
+ccdvis.seeing, ccdvis.visitId
+FROM dp02_dc2_catalogs.DiaSource AS diasrc JOIN dp02_dc2_catalogs.CcdVisit AS ccdvis
+ON diasrc.ccdVisitId = ccdvis.ccdVisitId
+WHERE CONTAINS(POINT('ICRS', diasrc.ra, diasrc.decl), CIRCLE('ICRS', 67.4579, -44.0802, 0.0006))=1
+AND diasrc.filterName = 'i'`
+                },
+                {
+                    description: 'Query for processed visit images (calexps) that overlap the coordinate (62, -37) and were obtained in the r-band filter between modified Julian dates 60900 and 61000.',
+                    statement:
+                        `SELECT dataproduct_type, dataproduct_subtype, calib_level, lsst_band, em_min, em_max, 
+lsst_tract, lsst_patch, lsst_visit, lsst_filter, lsst_detector, t_exptime, t_min, t_max, s_ra, s_dec, s_fov, 
+obs_id, obs_collection, o_ucd, facility_name, instrument_name, s_region, access_url, access_format 
+FROM ivoa.ObsCore 
+WHERE CONTAINS(POINT('ICRS', 62, -37), s_region)=1 
+AND calib_level = 2 
+AND dataproduct_type = 'image' 
+AND ( t_min <= 61000 AND 60900 <= t_max ) 
+AND ( 600e-9 BETWEEN em_min AND em_max )`
+                },
+            ]
+        });
+};
