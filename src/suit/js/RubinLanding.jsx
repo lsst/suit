145b516f2e45884abc3f4914b87959a43919de4e
roby <roby@ipac.caltech.edu>
2024-02-20 11:53:48 -0700

    UI conversion
        - new side bar config
        - new landing page (Jaladh)
        - cleanup

---
+++ src/suit/js/RubinLanding.jsx
@@ -0,0 +1,24 @@
+import {Stack, Typography} from '@mui/joy';
+import React from 'react';
+import PORTAL_SYMBOL from '../html/images/portal-symbol.svg';
+import {LandingPage} from 'firefly/templates/fireflyviewer/LandingPage';
+
+
+const RubinBranding = ({}) => (
+    <Stack alignItems='center'>
+        <Typography sx={{fontSize: 'xl4'}} color='neutral'>
+            Rubin Science Platform Portal
+        </Typography>
+    </Stack>
+);
+
+export function RubinLanding() {
+    return (
+        <LandingPage
+            slots={{ topSection: RubinBranding }}
+            slotProps={{
+                bgMonitorHint: {sx: {right: 80}},
+                bottomSection: {icon: <img src={PORTAL_SYMBOL} alt={'Rubin Portal Symbol'} style={{width: '14rem'}}/>}
+        }}/>
+    );
+}
--- src/suit/js/SUIT.js
+++ src/suit/js/SUIT.js
@@ -1,6 +1,8 @@
 /*
  * License information at https://github.com/Caltech-IPAC/firefly/blob/master/License.txt
  */
+import {TapSearchPanel} from 'firefly/ui/tap/TapSearchRootPanel.jsx';
+import React from 'react';
 import {firefly} from 'firefly/Firefly.js';
 import {
     makeDefImageSearchActions,
@@ -10,9 +12,16 @@ import {mergeObjectOnly, getRootURL} from 'firefly/util/WebUtil.js';
 import {getTAPServices} from 'firefly/ui/tap/TapKnownServices.js';
 import {getFireflyViewerWebApiCommands} from 'firefly/api/webApiCommands/ViewerWebApiCommands.js';
 import './suit.css';
-import {makeLsstClickToAction, makeLsstTapEntry} from './actions.jsx';
+import {makeLsstClickToAction, makeLsstTapEntry, LSST_DP02_DC2, LSST_DP03_SSO} from './actions.jsx';
+import {RubinLanding} from './RubinLanding.jsx';
 
 // import SUIT_ICO from 'html/images/rubin_logo_transparent-70.png';
+
+const OTHER_CAT= 'Other archive searches';
+
+const RUBIN= 'Rubin searches';
+const LSST_DP02_DC2_IMAGES= LSST_DP02_DC2+'-images';
+// const LSST_DP03_SSO_IMAGES=LSST_DP03_SSO+'-images';
 
 /**
  * This entry point is customized for LSST suit.  Refer to FFEntryPoint.js for information on
@@ -22,20 +31,51 @@ let props = {
     showUserInfo: true,
     // appIcon: getRootURL() +'images/rubin_logo_transparent-40.png',
     appIcon: getRootURL() +'images/rubin-favicon-transparent-45px.png',
-    bannerLeftStyle:{margin: '-2px 5px 0 10px'},
+    bannerLeftStyle:{margin: '3px 10px 0 10px'},
     showViewsSwitch: true,
-    // rightButtons: [timeSeriesButton],
     menu: [
-        {label: 'RSP TAP Search', action: 'TAPSearch'},
-        {label: 'External Images', action: 'ImageSelectDropDownCmd'},
-        {label: 'External Catalogs', action: 'MultiTableSearchCmd'},
-        {label: 'Add Chart', action: 'ChartSelectDropDownCmd'},
-        {label: 'Upload', action: 'FileUploadDropDownCmd'}
+        {label: 'DP0.2 Images', action: LSST_DP02_DC2_IMAGES, primary:true, category:RUBIN,
+            title: 'Search DP0.2 Images'},
+        {label: 'DP0.2 Catalogs', action: LSST_DP02_DC2, primary:true, category:RUBIN,
+            title: 'Search DP0.2 catalogs'},
+        {label: 'DP0.3 Catalogs', action: LSST_DP03_SSO, primary:true, category:RUBIN,
+            title: 'Search DP0.3 catalogs'},
+
+        {label:'General TAP', action: 'TAPSearch', category:OTHER_CAT},
+        {label: 'IRSA Images', action: 'ImageSelectDropDownCmd', category: OTHER_CAT},
+        {label: 'IRSA Catalogs', action: 'MultiTableSearchCmd', category: OTHER_CAT},
+        {label:'NED Objects', action: 'ClassicNedSearchCmd', primary: false, category:OTHER_CAT},
+        {label:'VO Cone Search', action: 'ClassicVOCatalogPanelCmd', primary: false, category: OTHER_CAT},
+
+        {label: 'Upload', action: 'FileUploadDropDownCmd', primary:true}
     ],
-    appTitle: 'Rubin Portal'
+    appTitle: 'Rubin Portal',
+    landingPage: <RubinLanding/>,
+    fileDropEventAction: 'FileUploadDropDownCmd',
+
+
+    dropdownPanels: [
+        <TapSearchPanel lockService={true} lockedServiceName={LSST_DP02_DC2} groupKey={LSST_DP02_DC2}
+                        layout= {{width: '100%'}}
+                        name={LSST_DP02_DC2}/>,
+        <TapSearchPanel lockService={true} lockedServiceName={LSST_DP02_DC2_IMAGES} groupKey={LSST_DP02_DC2_IMAGES}
+                        lockObsCore={true} obsCoreLockTitle='DP0.2 Image Search via ObsTAP'
+                        layout= {{width: '100%'}}
+                        name={LSST_DP02_DC2_IMAGES}/>,
+        <TapSearchPanel lockService={true} lockedServiceName={LSST_DP03_SSO} groupKey={LSST_DP03_SSO}
+                        layout= {{width: '100%'}}
+                        name={LSST_DP03_SSO}/>,
+        // <TapSearchPanel lockService={true} lockedServiceName={LSST_DP03_SSO_IMAGES} groupKey={LSST_DP03_SSO_IMAGES}
+        //                 lockObsCore={true}
+        //                 layout= {{width: '100%'}}
+        //                 name={LSST_DP03_SSO_IMAGES}/>,
+    ],
+
+
+
 };
 
-const LSST_TAP_LABEL= 'LSST DP0.2 DC2';
+// const LSST_TAP_LABEL= 'LSST DP0.2 DC2';
 
 props = mergeObjectOnly(props, window.firefly?.app ?? {});
 
@@ -83,7 +123,7 @@ let options = {
     },
     tapObsCore: {
         enableObsCoreDownload: true, // enable for other obscore
-        [LSST_TAP_LABEL]  : {
+        [LSST_DP02_DC2]  : {
             enableObsCoreDownload: false, //disable for the portal
             filterDefinitions: [
                 {
