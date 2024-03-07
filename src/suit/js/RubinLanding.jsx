e0177e9bdc90532e7c3b6385c0e504a61cee0071
Jaladh Singhal <jaladhsinghal@gmail.com>
2024-03-07 15:05:56 -0800

    FIREFLY-1416: Fix the slots/slotProps design

--- src/suit/js/RubinLanding.jsx
+++ src/suit/js/RubinLanding.jsx
@@ -4,17 +4,18 @@ import PORTAL_SYMBOL from '../html/images/portal-symbol.svg';
 import {LandingPage} from 'firefly/templates/fireflyviewer/LandingPage';
 
 
+const RubinBranding = ({}) => (
+    <Stack alignItems='center'>
+        <Typography sx={{fontSize: 'xl4'}} color='neutral'>
+            Rubin Science Platform Portal
+        </Typography>
+    </Stack>
+);
+
 export function RubinLanding() {
     return (
         <LandingPage
-            slots={{
-                topSection: (
-                    <Stack alignItems='center'>
-                        <Typography sx={{fontSize: 'xl4'}} color='neutral'>
-                            Rubin Science Platform Portal
-                        </Typography>
-                    </Stack>
-                )}}
+            slots={{ topSection: RubinBranding }}
             slotProps={{
                 bgMonitorHint: {sx: {right: 80}},
                 bottomSection: {icon: <img src={PORTAL_SYMBOL} alt={'Rubin Portal Symbol'} style={{width: '14rem'}}/>}
