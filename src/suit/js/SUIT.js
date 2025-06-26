1a2900d8ca0cc7e9059dcf70ba6fd1c20fc96ab6
roby <roby@ipac.caltech.edu>
2025-06-26 15:57:38 -0600

    work for rubin release

--- src/suit/js/SUIT.js
+++ src/suit/js/SUIT.js
@@ -19,7 +19,9 @@ import {getTAPServicesByName} from 'firefly/ui/tap/TapKnownServices.js';
 import {getFireflyViewerWebApiCommands} from 'firefly/api/webApiCommands/ViewerWebApiCommands.js';
 import {
     makeLsstClickToAction, makeLsstTapEntry, LSST_DP02_DC2, LSST_DP03_SSO, makeLsstSiaEntry, LSST_DP02_SIAV2_DC2,
-    RUBIN_DP02_DC2_ID, RUBIN_DP03_SSO_ID, RUBIN_LIVE_OBSCORE_ID, RUBIN_SIA_DP0_ID, getAlternateColorScheme
+    RUBIN_PRIMARY_TAP_ID,
+    RUBIN_DP03_SSO_ID, RUBIN_LIVE_OBSCORE_ID, getAlternateColorScheme, RUBIN_DP1,
+    LSST_DP02_SIAV2_DC2_ID, RUBIN_DP1_SIAV2, RUBIN_DP1_SIAV2_ID, RUBIN_PRIMARY_TAP
 } from './actions.jsx';
 import {getRubinDCECollectionAttributes, makeRubinDCERegistryRequest} from './RubinInventoryConfig';
 import {RubinLanding, RubinLandingAPI} from './RubinLanding.jsx';
@@ -27,10 +29,11 @@ import {RubinLanding, RubinLandingAPI} from './RubinLanding.jsx';
 import APP_ICON from '../html/images/rubin-favicon-transparent-45px.png';
 import './suit.css';
 
-const OTHER_CAT= 'Other archive searches';
 
-const RUBIN= 'Rubin searches';
-const LSST_DP02_DC2_IMAGES= LSST_DP02_DC2+'-images';
+const DP1= 'Data Preview 1';
+const DP0= 'Data Preview 0';
+const OTHER_CAT= 'Other archive searches';
+const RUBIN_DP1_IMAGES= RUBIN_DP1+'-images';
 const LSST_DP02_DC2_SIAV2_IMAGES= LSST_DP02_DC2+'-siaV2images';
 // const LSST_DP03_SSO_IMAGES=LSST_DP03_SSO+'-images';
 
@@ -43,19 +46,21 @@ let props = {
     appIcon: <img src={APP_ICON} style={{width:36}}/>,
     showViewsSwitch: true,
     menu: [
-        {label:'Data Collections', action:'RubinDataCollections', primary: menuItemPrimary,  category:RUBIN, enabled:menuItemEnabled},
-        {label: 'DP0.2 Images', action: LSST_DP02_DC2_IMAGES, primary:menuItemPrimary, category:RUBIN,
-            title: 'Search DP0.2 Images',  enabled:menuItemEnabled},
-        {label: 'DP0.2 Images SIAv2', action: LSST_DP02_DC2_SIAV2_IMAGES, primary:menuItemPrimary, category:RUBIN,
-            title: 'Search DP0.2 Images SIAv2',  enabled:menuItemEnabled},
-        {label: 'DP0.2 Catalogs', action: LSST_DP02_DC2, primary:menuItemPrimary, category:RUBIN,
-            title: 'Search DP0.2 catalogs',  enabled:menuItemEnabled},
-        {label: 'DP0.3 Catalogs', action: LSST_DP03_SSO, primary:menuItemPrimary, category:RUBIN,
-            title: 'Search DP0.3 catalogs', enabled:menuItemEnabled},
-        {label:'Rubin HiPS Search', action: 'HiPSSearchPanel', primary: false, category:RUBIN,enabled:menuItemEnabled },
+        {label:'Rubin HiPS Search', action: 'HiPSSearchPanel', primary: menuItemPrimary, category:DP1,enabled:menuItemEnabled},
+        {label: 'DP1 Images', action: 'rubin-obscore-images-dp1', primary:menuItemPrimary, category:DP1,
+            title: 'Search DP1 Images',  enabled:menuItemEnabled},
+        {label: 'DP1 Images SIAv2', action: RUBIN_DP1_SIAV2_ID, primary:menuItemPrimary, category: DP1,enabled:menuItemEnabled},
+        {label: 'DP1 Catalogs', action: 'rubin-catalogs-dp1', primary:menuItemPrimary, category:DP1,
+            title: 'Search DP1 catalogs',  enabled:menuItemEnabled},
 
-        {label: 'General TAP', action: 'TAPSearch', category:OTHER_CAT},
-        {label: 'SIAv2 Searches', action: 'SIAv2Search', primary:false, category: OTHER_CAT},
+
+        {label: 'DP0 Images', action: 'rubin-obscore-images-dp0', primary:menuItemPrimary, category: DP0,enabled:menuItemEnabled},
+        {label: 'DP0 Images SIAv2', action: 'dp02-siav2-images', primary:menuItemPrimary, category: DP0,enabled:menuItemEnabled},
+
+        {label: 'Gaia TAP at EASC', action: 'gaia-tap', primary:menuItemPrimary, category: OTHER_CAT,enabled:menuItemEnabled},
+
+        {label: 'Multi-archive TAP', action: 'TAPSearch', category:OTHER_CAT, primary:false},
+        {label: 'Multi-archive SIAv2', action: 'SIAv2Search', primary:false, category: OTHER_CAT},
         {label: 'IRSA Images', action: 'ImageSelectDropDownCmd', category: OTHER_CAT},
         {label: 'IRSA Catalogs', action: 'IrsaCatalog',  category:OTHER_CAT},
         {label: 'NED Objects', action: 'ClassicNedSearchCmd', primary: false, category:OTHER_CAT},
@@ -69,24 +74,32 @@ let props = {
 
 
     dropdownPanels: [
-        <TapSearchPanel lockService={true} lockedServiceName={LSST_DP02_DC2} groupKey={LSST_DP02_DC2}
-                        layout= {{width: '100%'}}
-                        name={LSST_DP02_DC2}/>,
-        <TapSearchPanel lockService={true} lockedServiceName={LSST_DP02_DC2_IMAGES} groupKey={LSST_DP02_DC2_IMAGES}
-                        lockObsCore={true} obsCoreLockTitle='DP0.2 Image Search via ObsTAP'
-                        layout= {{width: '100%'}}
-                        name={LSST_DP02_DC2_IMAGES}/>,
-        <TapSearchPanel lockService={true} lockedServiceName={LSST_DP03_SSO} groupKey={LSST_DP03_SSO}
-                        layout= {{width: '100%'}}
-                        name={LSST_DP03_SSO}/>,
+        <TapSearchPanel lockService={true} lockedServiceName={RUBIN_PRIMARY_TAP} groupKey={RUBIN_DP1_IMAGES}
+                        layout= {{width: '100%'}} name={RUBIN_DP1_IMAGES}/>,
+        <TapSearchPanel lockService={true} lockedServiceName={RUBIN_PRIMARY_TAP}
+                        groupKey='rubin-obscore-images-dp1' name='rubin-obscore-images-dp1'
+                        lockObsCore={true}
+                        obsCoreLockTitle='DP1 Image Search via ObsTAP' layout= {{width: '100%'}}/>,
+        <TapSearchPanel lockService={true} lockedServiceName={RUBIN_PRIMARY_TAP}
+                        lockedSchemaName='dp1' obsCoreLockTitle='DP1 Catalogs'
+                        groupKey='rubin-catalogs-dp1' name='rubin-catalogs-dp1'
+                        layout= {{width: '100%'}}/>,
+        <SIAv2SearchPanel lockService={true} lockedServiceName={RUBIN_DP1_SIAV2_ID} groupKey={RUBIN_DP1_SIAV2_ID}
+                          layout= {{width: '100%'}} lockTitle='DP1 Images SIAv2' name={RUBIN_DP1_SIAV2_ID}/>,
         <SIAv2SearchPanel lockService={true} lockedServiceName={LSST_DP02_SIAV2_DC2} groupKey='LSST_DP02_DC2_SIAV2_IMAGES'
                           layout= {{width: '100%'}}
                           lockTitle='DP0.2 Image Search via SIAv2 Search'
-                          name={LSST_DP02_DC2_SIAV2_IMAGES}/>,
-        // <TapSearchPanel lockService={true} lockedServiceName={LSST_DP03_SSO_IMAGES} groupKey={LSST_DP03_SSO_IMAGES}
-        //                 lockObsCore={true}
-        //                 layout= {{width: '100%'}}
-        //                 name={LSST_DP03_SSO_IMAGES}/>,
+                          name='dp02-siav2-images'/>,
+        <TapSearchPanel lockService={true} lockedServiceName={RUBIN_PRIMARY_TAP}
+                        groupKey='rubin-obscore-images-dp0' name='rubin-obscore-images-dp0'
+                        lockObsCore={true}
+                        lockedSchemaName='dp02_dc2_catalogs'
+                        lockedTableName='dp02_dc2_catalogs.ObsCore'
+                        obsCoreLockTitle='DP0 Image Search via ObsTAP' layout= {{width: '100%'}}/>,
+
+        <TapSearchPanel lockService={true} lockedServiceName='Gaia' groupKey={'gaia-tap'}
+                        layout= {{width: '100%'}} name='gaia-tap'/>,
+
         <DLGeneratedDropDown {...{
             name:'RubinDataCollections',
             key:'RubinDataCollections',
@@ -104,12 +117,17 @@ const alwaysEnabled= [
         'SIAv2Search', 'FileUploadDropDownCmd'];
 
 const defPrimaryMenuList= [
-    'RubinDataCollections', LSST_DP02_DC2_IMAGES, LSST_DP02_DC2_SIAV2_IMAGES, LSST_DP02_DC2, LSST_DP03_SSO,
+    'rubin-obscore-images-dp1', 'rubin-catalogs-dp1',
+    'rubin-obscore-images-dp1',
+    'RubinDataCollections', RUBIN_DP1_IMAGES, LSST_DP02_DC2_SIAV2_IMAGES, LSST_DP02_DC2,
+    RUBIN_DP1,
 ];
 
 const defEnabledMenuList= [
-    'RubinDataCollections', LSST_DP02_DC2_IMAGES, LSST_DP02_DC2_SIAV2_IMAGES, LSST_DP02_DC2, LSST_DP03_SSO,
-    'HiPSSearchPanel'
+    'rubin-obscore-images-dp1', 'rubin-catalogs-dp1', 'dp02-siav2-images', 'rubin-obscore-images-dp0',
+    'RubinDataCollections', 'gaia-tap',
+    LSST_DP02_DC2_SIAV2_IMAGES, LSST_DP02_DC2, LSST_DP03_SSO,
+    'HiPSSearchPanel', RUBIN_DP1_SIAV2_ID, RUBIN_DP1
 ];
 
 
@@ -136,19 +154,21 @@ if (!props?.template) { // api mode
 
 
 const tapServices=  [
-    makeLsstTapEntry(),
+    makeLsstTapEntry(RUBIN_PRIMARY_TAP_ID, RUBIN_PRIMARY_TAP, 'https://data-int.lsst.cloud/api/tap'),
+    makeLsstTapEntry('RubinDp03SSO', 'LSST DP0.3 SSO', 'https://data-int.lsst.cloud/api/ssotap'),
     ...getTAPServicesByName( ['IRSA', 'Gaia', 'CADC', 'MASTImages', 'GAVO', 'HSA', 'NED',
         'VizieR', 'Simbad', 'ExoplanetArchive'])
 ];
 
-const siaServices=  [
-    makeLsstSiaEntry(),
-    ...getSIAv2ServicesByName( ['IRSA', 'CADC']),
+const siaServices = [
+    makeLsstSiaEntry(RUBIN_DP1_SIAV2, RUBIN_DP1_SIAV2_ID, 'https://data-int.lsst.cloud/api/sia/dp1/query'),
+    makeLsstSiaEntry(LSST_DP02_SIAV2_DC2_ID, LSST_DP02_SIAV2_DC2, 'https://data-int.lsst.cloud/api/sia/dp02/query'),
+    ...getSIAv2ServicesByName(['IRSA', 'CADC']),
 ];
 
 const rubinDataServiceOptions= {
-    targetPanelExampleRow1: ['62, -37', '60.4 -35.1', '4h11m59s -32d51m59s equ j2000', '239.2 -47.6 gal'],
-    targetPanelExampleRow2: ['NGC 1532', '(NB: DC2 is a simulated sky, so names are not useful)'],
+    targetPanelExampleRow1: ['40.07, -34.4', '106.1, -10.45', '37.83, 6.95'],
+    targetPanelExampleRow2: ['62, -37', '60.4 -35.1', '4h11m59s -32d51m59s equ j2000', '239.2 -47.6 gal'],
     enableObsCoreDownload: false, //disable for the portal
     cutoutDefSizeDeg: .02,
     enableMetadataLoad: true, // loads metadata for columns to generate input field options
@@ -212,8 +232,8 @@ let options = {
     },
     coverage : { // example of using DSS and wise combination for coverage (not that anyone would want to combination)
         // Use a server that is purely internal to the RSP, pending authentication-flow changes:
-        hipsSourceURL : 'https://data-int.lsst.cloud/api/hips/images/color_gri',
-        hipsSource360URL : 'https://data-int.lsst.cloud/api/hips/images/color_gri',
+        hipsSourceURL : 'https://data-int.lsst.cloud/api/hips/v2/dp1/deep_coadd/band_r',
+        hipsSource360URL : 'https://data-int.lsst.cloud/api/hips/v2/dp1/deep_coadd/band_r',
         fovDegFallOver: .00001, // small number will never show an image only a HiPS
         exclusiveHiPS: true,
         imageSourceParams: { //use 2mass if the user forces an image request
@@ -238,9 +258,19 @@ let options = {
         enableObsCoreDownload: true, // enable for other obscore
         preferCutout: false,
         ...rubinDataServiceOptions,
-        [RUBIN_DP02_DC2_ID]  : {},
+        [RUBIN_PRIMARY_TAP_ID]  : {
+            targetPanelExampleRow1: ['40.07, -34.4', '106.1, -10.45', '37.83, 6.95'],
+            targetPanelExampleRow2: ['53.25, -28.089 EQ_J2000', '62, -37'],
+        },
         [RUBIN_DP03_SSO_ID]  : {},
-        [RUBIN_SIA_DP0_ID]  : {},
+        [LSST_DP02_SIAV2_DC2_ID]  : {
+            targetPanelExampleRow1: ['62, -37', '60.4 -35.1', '4h11m59s -32d51m59s equ j2000', '239.2 -47.6 gal'],
+            targetPanelExampleRow2: ['NGC 1532', '(NB: DC2 is a simulated sky, so names are not useful)'],
+        },
+        [RUBIN_DP1_SIAV2_ID] : {
+            targetPanelExampleRow1: ['40.07, -34.4', '106.1, -10.45', '37.83, 6.95'],
+            targetPanelExampleRow2: ['53.25, -28.089 EQ_J2000', '62, -37'],
+        },
         [RUBIN_LIVE_OBSCORE_ID]  : {},
         'rubin'  : {},
     },
@@ -291,7 +321,9 @@ firefly.bootstrap(props, options,
     getFireflyViewerWebApiCommands(undefined,
         [
             {desc:LSST_DP02_DC2, name:LSST_DP02_DC2},
-            {desc:LSST_DP02_DC2_IMAGES, name:LSST_DP02_DC2_IMAGES},
-            {desc:LSST_DP03_SSO, name:LSST_DP03_SSO},
+            {desc:'DP1 Images', name:'rubin-obscore-images-dp1'},
+            {desc:'DP1 Catalogs', name:'rubin-catalogs-dp1'},
+            {desc:'DP0 Images', name:'rubin-obscore-images-dp0'},
+            {desc:'Gaia TAP at EASC', name:'gaia-tap'},
         ],
     ));
